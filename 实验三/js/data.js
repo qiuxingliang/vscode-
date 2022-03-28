c658fa694fbddcaba6ca478212dc2ec8456142c1
qiuxingliang <1901440679@qq.com>
2022-03-28 22:01:56 +0800

    添加实验三

---
+++ 实验三/js/data.js
@@ -0,0 +1,169 @@
+var shopcarts = [
+    {
+        // 购物车编号
+        cartId: 1,
+        // 店铺编号 
+        shopId: 2001,
+        // 店铺名称
+        shopName: "荣耀官方旗舰店",
+        ischeck:false,
+        products: [
+            {
+                // 商品编号
+                productId: 1001,
+                // 商品名称
+                productName: "【64GB限时优惠100元】华为旗下荣耀Play4T手机新品大电池AI摄影",
+                // 顶层图片
+                topImg: "img/4.jpg",
+                // 商品图片
+                imgs: [
+                    "img/1.jpg",
+                    "img/2.jpg",
+                    "img/3.jpg",
+                    "img/4.jpg",
+                ],
+                // 是否参加电竞节
+                isActivity: true,
+                // 是否支持信用卡
+                isCard: true,
+                // 是否支持七天退货
+                isSeven: true,
+                // 是否如实描述
+                isRealDesc: true,
+                // 规格
+                spec: `<p>机身颜色：iPhone8 银色4.7英寸</p>
+                <p>版本类型：中国大陆</p>
+                <p>套餐类型：官方标配</p>
+                <p>存储容量：256GB</p>`,
+                marketPrice: 2000,
+                // 单价
+                price: 1199.00,
+                // 购买的数量
+                count: 2,
+                // 金额
+                amount: 2398.00
+            },
+            {
+                // 商品编号
+                productId: 1002,
+                // 商品名称
+                productName: "【6+64GB魅海蓝限时低至1199元】华为旗下荣耀手机荣耀9X麒麟",
+                // 顶层图片
+                topImg: "img/5.jpg",
+                // 商品图片
+                imgs: [
+                    "img/1.jpg",
+                    "img/2.jpg",
+                    "img/3.jpg",
+                    "img/4.jpg",
+                ],
+                // 是否参加电竞节
+                isActivity: false,
+                // 是否支持信用卡
+                isCard: true,
+                // 是否支持七天退货
+                isSeven: true,
+                // 是否如实描述
+                isRealDesc: true,
+                // 规格
+                spec: `<p>机身颜色：iPhone8 银色4.7英寸</p>
+                <p>版本类型：中国大陆</p>
+                <p>套餐类型：官方标配</p>
+                <p>存储容量：256GB</p>`,
+                marketPrice: 2000,
+                // 单价
+                price: 1199.00,
+                // 购买的数量
+                count: 2,
+                // 金额
+                amount: 2398.00
+            }
+        ]
+    },
+    {
+        // 购物车编号
+        cartId: 2,
+        // 店铺编号 
+        shopId: 2002,
+        // 店铺名称
+        shopName: "罗蒙派大星专卖店",
+        ischeck:false,
+        products: [
+            {
+                // 商品编号
+                productId: 1003,
+                // 商品名称
+                productName: "罗蒙夏季休闲裤男薄款大码长裤宽松直筒男裤冰丝超薄西裤男士裤子",
+                // 顶层图片
+                topImg: "img/6.jpg",
+                // 商品图片
+                imgs: [
+                    "img/1.jpg",
+                    "img/2.jpg",
+                    "img/3.jpg",
+                    "img/4.jpg",
+                ],
+                // 是否参加电竞节
+                isActivity: false,
+                // 是否支持信用卡
+                isCard: true,
+                // 是否支持七天退货
+                isSeven: true,
+                // 是否如实描述
+                isRealDesc: true,
+                // 规格
+                spec: "<p>颜色：宝蓝色【5023款】+宝蓝色【5023】</p><p>尺码：28[腰围2尺10]</p>",
+                marketPrice: 200,
+                // 单价
+                price: 168.00,
+                // 购买的数量
+                count: 2,
+                // 金额
+                amount: 336.00
+            }
+        ]
+    },
+    {
+        // 购物车编号
+        cartId: 3,
+        // 店铺编号 
+        shopId: 2003,
+        // 店铺名称
+        shopName: "雅鹿提趣专卖店",
+        ischeck:false,
+        products: [
+            {
+                // 商品编号
+                productId: 1004,
+                // 商品名称
+                productName: "雅鹿天丝休闲裤男士夏季薄款商务修身长裤子直筒西裤夏天冰丝男裤",
+                // 顶层图片
+                topImg: "img/7.jpg",
+                // 商品图片
+                imgs: [
+                    "img/1.jpg",
+                    "img/2.jpg",
+                    "img/3.jpg",
+                    "img/4.jpg",
+                ],
+                // 是否参加电竞节
+                isActivity: false,
+                // 是否支持信用卡
+                isCard: true,
+                // 是否支持七天退货
+                isSeven: true,
+                // 是否如实描述
+                isRealDesc: true,
+                // 规格
+                spec: "<p>颜色：660卡其(单件)</p><p>尺码：29</p>",
+                marketPrice: 200,
+                // 单价
+                price: 118.00,
+                // 购买的数量
+                count: 1,
+                // 金额
+                amount: 118.00
+            }
+        ]
+    }
+]
